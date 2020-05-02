ff03fdd6470b259d52b4fb58103d54a075d91498
RTHAKUR3 <rthakur3@ford.com>
2020-05-02 16:11:33 -0400

    Spiral Matrix

--- JavaScript-DataStructures/exercises/matrix/index.js
+++ JavaScript-DataStructures/exercises/matrix/index.js
@@ -15,6 +15,49 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+    let startRow =0, startCol = 0;
+    let endRow= n-1, endCol = n-1;
+    let results = [];
+    for(let i=0; i< n; i++){
+        results.push([]);
+    }
+    let counter = 1;
+
+    while(startCol <= endCol && startRow <= endRow){
+        // Top row
+        for(let i= startCol; i <=endCol ; i++){
+            results[startRow][i] = counter;
+            counter++;
+        }
+        console.log(results);
+        startRow++;
+        // Right col
+        for(let i = startRow; i<=endRow; i++){
+            results[i][endCol] = counter;
+            counter++;
+        }
+        console.log(results);
+        endCol--;
+        //Bottom row
+        for(let i = endCol; i>=startCol ; i--){
+            results[endRow][i] = counter;
+            counter++;
+        }
+        console.log(results);
+        endRow--;
+        // Start Col
+        for(let i =endRow; i >=startRow ; i--){
+            results[i][startCol] = counter;
+            counter++;
+        }
+        startCol++;
+    }
+
+    return results;
+}
+
+console.log(matrix(3));
+
 
 module.exports = matrix;
