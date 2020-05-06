41115891d025500d07a274a7fb452ab7a165f19f
RTHAKUR3 <rthakur3@ford.com>
2020-05-05 23:05:43 -0400

    Fibonacci and Queue

--- JavaScript-DataStructures/exercises/fib/index.js
+++ JavaScript-DataStructures/exercises/fib/index.js
@@ -8,6 +8,46 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+// function fib(n) {
+//     let result = 1;
+//     let arrayResults = [0,1];
+  
+//     for(let i = 2; i<= n; i++){
+//         result += arrayResults[i-2]
+//         arrayResults.push(result);
+//     }
+//     console.log(arrayResults);
+//     return arrayResults[n];
+// }
+
+function memoize(fn){
+    const cache = {};
+    return function(...args){
+        if(cache[args]){
+            return cache[args];
+        }
+        const result = fn.apply(this, args);
+        cache[args] = result;
+        return result;
+    }
+}
+
+function fib(n) {
+    // we can use this as well
+    // if(n < 2){
+    //     return n
+    // }
+    if (n === 0) {
+        return 0;
+    }
+    if (n === 1) {
+        return 1;
+    }
+
+    return fib(n - 1) + fib(n - 2);
+}
+
+fib = memoize(fib);
 
 module.exports = fib;
+
